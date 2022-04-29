e66464a625c1dd2aa89f6cddb5eb45ffeec10b3c
FredrikStigen <fredrikstigen@yahoo.no>
2022-04-29 15:24:37 +0200

    .

---
+++ Controller/encoder/rotary_encoder.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import pigpio
+import time
+
+
+class decoder:
+
+   """Class to decode mechanical rotary encoder pulses."""
+
+   def __init__(self, pi, gpioA, gpioB, callback):
+
+      self.pi = pi
+      self.gpioA = gpioA
+      self.gpioB = gpioB
+      self.callback = callback
+
+      self.levA = 0
+      self.levB = 0
+
+      self.lastGpio = None
+
+      self.pi.set_mode(gpioA, pigpio.INPUT)
+      self.pi.set_mode(gpioB, pigpio.INPUT)
+
+      self.pi.set_pull_up_down(gpioA, pigpio.PUD_UP)
+      self.pi.set_pull_up_down(gpioB, pigpio.PUD_UP)
+
+      self.cbA = self.pi.callback(gpioA, pigpio.EITHER_EDGE, self._pulse)
+      self.cbB = self.pi.callback(gpioB, pigpio.EITHER_EDGE, self._pulse)
+
+   def _pulse(self, gpio, level, tick):
+
+
+      if gpio == self.gpioA:
+         self.levA = level
+      else:
+         self.levB = level;
+
+      if gpio != self.lastGpio: # debounce
+         self.lastGpio = gpio
+
+         if   gpio == self.gpioA and level == 1:
+            if self.levB == 1:
+               self.callback(1)
+         elif gpio == self.gpioB and level == 1:
+            if self.levA == 1:
+               self.callback(-1)
+
+   def cancel(self):
+      self.cbA.cancel()
+      self.cbB.cancel()
+
+if __name__ == "__main__":
+   pos = 0
+
+   def callback(way):
+      global pos
+      if pos >= 400:
+         pos = 0
+      elif pos <= 0:
+         pos = 400
+      pos += way
+
+      print("pos={}".format(round(pos*0.9, 3)))
+
+   pi = pigpio.pi()
+
+   decoder = decoder(pi, 5, 6, callback)
+
+   time.sleep(300)
+
+   decoder.cancel()
+
+   pi.stop()
+
